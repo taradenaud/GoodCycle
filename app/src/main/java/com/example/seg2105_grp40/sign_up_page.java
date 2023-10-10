388df0973b192ff5e65fbc3721a9a68e9fc1e704
Matthew Lavoie <Mattplavoie03@gmail.com>
2023-10-10 19:41:14 -0400

    te

---
+++ app/src/main/java/com/example/seg2105_grp40/sign_up_page.java
@@ -0,0 +1,14 @@
+package com.example.seg2105_grp40;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sign_up_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_page);
+    }
+}
