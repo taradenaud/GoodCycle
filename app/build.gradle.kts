a66f363e03a3b5d0c367a74a8efad045e9a85c46
Connor States <123701849+ConnorStates0@users.noreply.github.com>
2023-10-10 16:12:14 -0400

    Database changes/test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,8 @@ android {
 
 dependencies {
 
+    implementation("com.google.firebase:firebase-bom:32.3.1")
+    implementation("com.google.firebase:firebase-database:20.2.2")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
--- app/src/main/java/com/example/segcyclingapp/MainActivity.java
+++ app/src/main/java/com/example/segcyclingapp/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.segcyclingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    protected void loginbuttonClick(){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
     }
 }
