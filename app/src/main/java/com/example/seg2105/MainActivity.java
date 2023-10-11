f32d53bd7b68e55cfcb558202b2b754ef102b00d
Connor States <123701849+ConnorStates0@users.noreply.github.com>
2023-10-10 20:29:30 -0400

    New version

---
+++ app/src/main/java/com/example/seg2105/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.seg2105;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button login = (Button) findViewById(R.id.login_button);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LoginPage.class));
+            }
+        });
+
+        Button signup = (Button) findViewById(R.id.sign_up_button);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SignUp.class));
+            }
+        });
+    }
+
+}
