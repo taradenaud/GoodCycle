e48e724459993e5f3c80059fbb83386e8d4e186b
Connor States <123701849+ConnorStates0@users.noreply.github.com>
2023-11-08 23:24:13 -0500

    Added user view functionality, ability to delete users working Needs back button in AdminManageUsers

--- app/src/main/java/com/example/seg2105/SignUp.java
+++ app/src/main/java/com/example/seg2105/SignUp.java
@@ -46,10 +46,12 @@ public class SignUp extends AppCompatActivity {
             public void onClick(View v) {
                 String username = String.valueOf(UserName.getText());
                 FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference newUserNameRef = database.getReference("users/"+username+"/username");
                 DatabaseReference newUserRoleRef = database.getReference("users/"+username+"/role");
                 DatabaseReference newUserEmailRef = database.getReference("users/"+username+"/email");
                 DatabaseReference newUserPasswordRef = database.getReference("users/"+username+"/password");
 
+                newUserEmailRef.setValue(username);
                 newUserEmailRef.setValue(String.valueOf(emailText.getText()));
                 newUserPasswordRef.setValue(String.valueOf(pwdText.getText()));
                 newUserRoleRef.setValue(role);
--- app/src/main/java/com/example/seg2105/admin_dashboard.java
+++ app/src/main/java/com/example/seg2105/admin_dashboard.java
@@ -2,7 +2,17 @@ package com.example.seg2105;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class admin_dashboard extends AppCompatActivity {
 
@@ -10,5 +20,15 @@ public class admin_dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.admin_dashboard);
+
+        Button ManageUsrs = (Button) findViewById(R.id.ManageUsrs);
+
+        ManageUsrs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent( admin_dashboard.this, AdminManageUsers.class));
+            }
+        });
+
     }
 }
