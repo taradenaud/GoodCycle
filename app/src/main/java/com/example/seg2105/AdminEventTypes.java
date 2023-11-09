1f2695e47cacc8bf4180489685afde0685928902
Connor States <123701849+ConnorStates0@users.noreply.github.com>
2023-11-09 01:32:48 -0500

    Event Types Page done Still need back buttons. This commit is missing taras design updates, will reincorporate :)

---
+++ app/src/main/java/com/example/seg2105/AdminEventTypes.java
@@ -0,0 +1,126 @@
+package com.example.seg2105;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+public class AdminEventTypes extends AppCompatActivity {
+
+    private ListView TypesList;
+    private ArrayList<String> ArrayList = new ArrayList<>();
+    private ArrayAdapter adapter;
+
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference ref;
+    private String NewInputtedName;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_event_types);
+
+        ref = FirebaseDatabase.getInstance().getReference("events/types");
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, ArrayList);
+        TypesList = findViewById(R.id.TypeList);
+        TypesList.setAdapter(adapter);
+
+        EditText selectedtype = findViewById(R.id.currentName);//Selection identifier at top of page
+        ref.addChildEventListener(new ChildEventListener() {
+
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {//Called when loading list
+                String type = snapshot.child("name").getValue(String.class);
+                ArrayList.add(type);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {//Refresh list as something was changed
+                if(ArrayList.size() > 0){
+                    ArrayList.clear();
+                }
+                String type = snapshot.child("name").getValue(String.class);
+                ArrayList.add(type);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) { //Something is Deleted from Database, all that needs to happen is remove deleted item from arraylist of users
+                for (int i = 0; i < ArrayList.size(); i++){ //Not efficient needs optimization
+                    if(ArrayList.get(i).equals(snapshot.child("name").getValue(String.class))){
+                        ArrayList.remove(i);
+                    }
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        TypesList.setItemsCanFocus(true);
+        TypesList.setOnItemClickListener(new AdapterView.OnItemClickListener() { //set selected user based on user list click
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String eventtype =  parent.getItemAtPosition(position).toString();
+                NewInputtedName = eventtype;
+                selectedtype.setText(NewInputtedName);
+            }
+        });
+
+        Button deleteType = (Button) findViewById(R.id.DeleteType);
+        deleteType.setOnClickListener(new View.OnClickListener() { //delete button functionality
+            @Override
+            public void onClick(View v) {
+                ref.child(NewInputtedName).removeValue();
+            }
+        });
+
+        Button renameType = (Button) findViewById(R.id.renametype);
+        EditText newname = (EditText) findViewById(R.id.newname);
+        renameType.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatabaseReference renameref = database.getReference("events/types/"+ NewInputtedName);
+                String updatedName = String.valueOf(newname.getText());
+                renameref.child("name").setValue(updatedName);
+            }
+        });
+
+        FloatingActionButton AddType = (FloatingActionButton) findViewById(R.id.AddTypeButton);
+
+        AddType.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText newtype = (EditText) findViewById(R.id.NewTypeText);
+                String name = String.valueOf(newtype.getText());
+                DatabaseReference newTypeNameRef = database.getReference("events/types/"+name+"/name");
+                newTypeNameRef.setValue(name);
+                newtype.setText("Add New Event Type");
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/example/seg2105/admin_dashboard.java
+++ app/src/main/java/com/example/seg2105/admin_dashboard.java
@@ -6,13 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 public class admin_dashboard extends AppCompatActivity {
 
@@ -21,7 +14,7 @@ public class admin_dashboard extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.admin_dashboard);
 
-        Button ManageUsrs = (Button) findViewById(R.id.ManageUsrs);
+        Button ManageUsrs = (Button) findViewById(R.id.ManageUsrsbtn);
 
         ManageUsrs.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,12 +23,12 @@ public class admin_dashboard extends AppCompatActivity {
             }
         });
 
-        Button events = (Button) findViewById(R.id.Events);
+        Button events = (Button) findViewById(R.id.EventTypesbtn);
 
         events.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent( admin_dashboard.this, AdminEvents.class));
+                startActivity(new Intent( admin_dashboard.this, AdminEventTypes.class));
             }
         });
 
